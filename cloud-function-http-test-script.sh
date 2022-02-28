6f3488a138ce69a04ba69d7a43174b967504fcf3
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-02-28 15:18:05 -0500

    fixed cloud function imports

---
+++ cloud-function-http-test-script.sh
@@ -0,0 +1,2 @@
+curl -X POST https://us-central1-athegiamedical.cloudfunctions.net/save_biometrics_to_firestore \
+ -H 'Content-Type: application/json' -d '{"data":{"patient_uuid":"aaabbbccdddeeefff", "timestamp":"2019-12-14 13:48:00.267830","payload":{"result":{"key1":"this is key 1", "key2":"this is key2"}}}}' 
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-from pandas import Series
 import datetime
-
-import os
 
 # Use the application default credentials
 cred = credentials.ApplicationDefault()
