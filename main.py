4f9b7fe396747fcade46d1d22de70cfbd9dec7ca
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-02-28 15:18:54 -0500

    fixed cloud function imports

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-import datetime
 
 # Use the application default credentials
 cred = credentials.ApplicationDefault()
